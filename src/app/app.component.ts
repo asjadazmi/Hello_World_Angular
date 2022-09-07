dd5b32f5207c7ccdbd03b9eba06c861b5d3f3e0c
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-09-07 19:17:38 +0530

    Added:Event Binding on clicking the image we can directly go the site mention site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+ 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello World';
-  imageurl="assets/Bridgelabz.png"
+  imageurl="assets/Bridgelabz.png";
+  url="https://www.bridgelabz.com/"
+onClick($event:any){
+  console.log("save button is clicked!",$event);
+  window.open(this.url,"_blank")
 }
+
+}
+
