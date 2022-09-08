0ac5bc52ab191eecf3e1de4073985c8c628dcf10
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-09-08 19:55:07 +0530

    Added:Applying validation on the input and printing the error message in the same line if user gives the Invalid name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,11 +10,22 @@ export class AppComponent {
   title = 'Hello World';
   imageurl="assets/Bridgelabz.png";
   url="https://www.bridgelabz.com/";
-  username:String="";
+  username:string="";
+  usererror:string="";
 
   onClick($event:any){
   console.log($event);
-  window.open(this.url,"_blank")
+  window.open(this.url,"_blank");
+
+}
+onInput($event:any){
+  console.log($event.data);
+  const nameregex=RegExp('^[A-Z]{1}[a-z]{2,}$')
+  if(nameregex.test(this.username)){
+    this.usererror="";
+    return;
+  }
+  this.usererror="Name is incorrect";
 }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
